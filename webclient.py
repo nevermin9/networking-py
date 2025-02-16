9be8901726faa279101a30998c4dbf92003d141c
nevermin9 <chizzza@gmail.com>
2025-02-16 14:22:36 +0100

    init repo; writing tcp client

---
+++ webclient.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+import sys
+import socket as s
+
+FORMAT = "ISO-8859-1"
+def webclient(host: str, port: str | int =80):
+    p = int(port)
+    socket = s.socket()
+    print(f"making request to {host}...")
+    socket.connect((host, p))
+    http_message = "\r\n".join([
+        "GET / HTTP/1.1",
+        f"Host: {host}",
+        "Connection: close",
+        "",
+        "",
+    ])
+    socket.send(http_message.encode(FORMAT))
+    buff_size = 1096
+    res = socket.recv(buff_size)
+    count = 1
+    while len(res):
+        print(res.decode(FORMAT))
+        res = socket.recv(buff_size)
+    print("-finsih-")
+
+
+
+args = sys.argv[1:]
+if 0 < len(args) < 3:
+    webclient(*args)
+else:
+    raise Exception("wrong number of parameters")
+
+
+
