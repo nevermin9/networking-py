66746230469cc3a8a457696833cd101b21d7003d
nevermin9 <chizzza@gmail.com>
2025-02-17 19:58:07 +0100

    feat: add ability to read payload in webserver.py

--- webclient.py
+++ webclient.py
@@ -6,19 +6,21 @@ FORMAT = "ISO-8859-1"
 def webclient(host: str, port: str | int =80):
     p = int(port)
     socket = s.socket()
-    print(f"making request to {host}...")
+    print(f"making request to {host, p}...")
     socket.connect((host, p))
+    payload = b'How are you doing, man?'
     http_message = "\r\n".join([
-        "GET / HTTP/1.1",
+        "POST / HTTP/1.1",
         f"Host: {host}",
         "Connection: close",
+        "Content-Type: text/plain",
+        f"Content-Length: {len(payload)}",
         "",
-        "",
+        payload.decode(),
     ])
-    socket.send(http_message.encode(FORMAT))
+    socket.sendall(http_message.encode(FORMAT))
     buff_size = 1096
     res = socket.recv(buff_size)
-    count = 1
     while len(res):
         print(res.decode(FORMAT))
         res = socket.recv(buff_size)
--- webserver.py
+++ webserver.py
@@ -4,39 +4,55 @@ import socket as s
 import sys
 
 
+FORMAT = "ISO-8859-1"
+
+def get_content_len(msg: str) -> int:
+    parts = msg.split("\r\n")
+    for p in parts:
+        if p.lower().startswith("content-length"):
+            return int(p.split(" ")[-1])
+    return 0
+
+
+def get_body(msg: str):
+    parts = msg.split("\r\n\r\n")
+    return parts[-1].strip()
+
 def webserver(port: str | int =28333):
     socket = s.socket()
     socket.setsockopt(s.SOL_SOCKET, s.SO_REUSEADDR, 1)
     socket.bind(("", int(port)))
     socket.listen()
-    print(f"Listening on locahost:{port}")
+    print(f"Listening on localhost:{port}")
     while True:
         new_socket, addressinfo = socket.accept()
         print(f"Accepted connection from {addressinfo}...")
+
         buff = bytearray(1096)
         view = memoryview(buff)
-        content = []
+        msg = []
+        content_len = 0
+        body = ""
+
         while True:
+            if content_len > 0:
+                body = get_body("".join(msg))
+                if len(bytes(body, FORMAT)) == content_len:
+                    break
+
             nbytes_recv = new_socket.recv_into(buff)
             if nbytes_recv == 0:
                 break
-            content.append(view[:nbytes_recv].tobytes().decode())
-            if content[-1].endswith("\r\n\r\n"):
+
+            msg.append(view[:nbytes_recv].tobytes().decode(FORMAT))
+
+            if content_len == 0:
+                content_len = get_content_len("".join(msg))
+
+            if msg[-1].endswith('\r\n\r\n'):
                 break
-        print(content)
-        final_content = "".join(content)
-        print(f"Received: {final_content}")
 
-        # data = b""
-        # while True:
-        #     chunk = new_socket.recv(1096)
-        #     if not chunk:
-        #         break
-        #     data += chunk
-        #     if data.endswith(b"\r\n\r\n"):
-        #         break
-
-        # print(f"Received: {data.decode()}")
+        print(body)
 
         txt = b"Hello, world"
         response = "\r\n".join([
@@ -46,7 +62,7 @@ def webserver(port: str | int =28333):
             "",
             "",
             txt.decode(),
-        ]).encode()
+        ]).encode(FORMAT)
         new_socket.sendall(response)
         print("Response is sent...")
         new_socket.close()
